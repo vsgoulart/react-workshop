02ec744a2c86574a789537f541f7291b9d254b2e
Vinicius Goulart <goulart.vs@gmail.com>
2018-11-26 23:07:48 +0100

    Clean slides project

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -1,93 +1,42 @@
-// Import React
 import React from 'react'
-
-// Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text,
-} from 'spectacle'
-
-// Import theme
+import { Deck, Heading, Slide } from 'spectacle'
 import createTheme from 'spectacle/lib/themes/default'
-
-// Require CSS
 require('normalize.css')
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: '#fbfbfb',
+    secondary: '#3a3a3a',
+    tertiary: '#e87f36',
+    quaternary: '#8b8b8b',
+    sky: '#4a90e2',
+    skyNight: '#304f95',
+    kakiSweet: '#ff8c3b',
+    skySummer: '#529ff9',
+    tomato: '#ff0000',
+    babyBlue: '#ecf4fc',
+    pacific: '#42c5cb',
+    pacificMint: '#cbf4f5',
+    storm: '#d8d8d8',
+    cloudy: '#f0f0f0',
+    pearlWhite: '#fbfbfb',
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: {
+      name: 'Open Sans',
+      googleFont: true,
+    },
   }
 )
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck theme={theme}>
+        <Slide>
+          <Heading size={1} fit caps lineHeight={1}>
+            React Workshop
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
         </Slide>
       </Deck>
     )
