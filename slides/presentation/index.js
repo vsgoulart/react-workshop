234cab709054c3f115677ca7a2d08cf04134f7d3
Vinicius Goulart <goulart.vs@gmail.com>
2018-11-27 01:53:47 +0100

    Add presentation first draft

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import { Deck, Heading, Slide } from 'spectacle'
+import {
+  Deck,
+  Heading,
+  Slide,
+  CodePane,
+  BlockQuote,
+  Quote,
+  Cite,
+  Link,
+} from 'spectacle'
 import createTheme from 'spectacle/lib/themes/default'
 require('normalize.css')
 
@@ -34,9 +43,107 @@ export default class Presentation extends React.Component {
     return (
       <Deck theme={theme}>
         <Slide>
-          <Heading size={1} fit caps lineHeight={1}>
+          <Heading size="1" fit caps lineHeight="1">
             React Workshop
           </Heading>
+        </Slide>
+        <Slide>
+          <Heading size="1" fit caps lineHeight="1">
+            JavaScript Concepts
+          </Heading>
+        </Slide>
+        <Slide>
+          <Heading size="2" textColor="tertiary">
+            let / const
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/letConst.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="2" textColor="tertiary">
+            let / const
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/letConst.result.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="2" textColor="tertiary" caps>
+            Arrow functions
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/arrowFunction.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="4" textColor="tertiary" caps>
+            Object / array destructuring, rest operator
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="20"
+            source={require('raw-loader!../assets/destructuring.example')}
+          />
+        </Slide>
+        <Slide align="flex-start">
+          <Heading size="2" textColor="tertiary" caps>
+            Classes
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/classes.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="2" textColor="tertiary" caps>
+            Template strings
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/templateStrings.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="2" textColor="tertiary">
+            map / filter / reduce
+          </Heading>
+          <CodePane
+            lang="javascript"
+            textSize="24"
+            source={require('raw-loader!../assets/arrayFunctions.example')}
+          />
+        </Slide>
+        <Slide>
+          <Heading size="1" fit caps lineHeight="1">
+            What is React?
+          </Heading>
+        </Slide>
+        <Slide bgColor="secondary">
+          <BlockQuote>
+            <Quote textSize="42" textColor="tertiary">
+              React is a declarative, efficient, and flexible JavaScript library
+              for building user interfaces. It lets you compose complex UIs from
+              small and isolated pieces of code called “components”.
+            </Quote>
+            <Cite>
+              <Link
+                href="https://reactjs.org/tutorial/tutorial.html#what-is-react"
+                textColor="skySummer"
+                target="_blank"
+              >
+                https://reactjs.org/tutorial/tutorial.html#what-is-react
+              </Link>
+            </Cite>
+          </BlockQuote>
         </Slide>
       </Deck>
     )
